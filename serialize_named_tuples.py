fe0694b5632ee82cc1a73c5e2bc1ac5b08661a26
Tomer Keren <tomer.keren.dev@gmail.com>
2019-04-20 23:32:06 +0300

    Declare function to create seralization hooks

---
+++ serialize_named_tuples.py
@@ -0,0 +1,10 @@
+from typing import Dict, Type, NamedTuple, Any, Callable, TypeVar
+
+NamedTuple_T = TypeVar("NamedTuple_T", bound=Type[NamedTuple])
+Json_Object = Dict[[str, Any]]
+
+
+def create_object_hook(
+    named_tuple_cls: Type[NamedTuple_T]
+) -> Callable[[Json_Object], NamedTuple_T]:
+    """Return a Object hook to seralize the given named tuple class from JSON"""
