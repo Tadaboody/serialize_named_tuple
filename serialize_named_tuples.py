29a84a971be2acee7eef408df866d19c02850c23
Tomer Keren <tomer.keren.dev@gmail.com>
2019-04-20 23:48:00 +0300

    Implement flat named tuple deserializing from a json

--- serialize_named_tuples.py
+++ serialize_named_tuples.py
@@ -4,7 +4,23 @@ NamedTuple_T = TypeVar("NamedTuple_T", bound=Type[NamedTuple])
 Json_Object = Dict[str, Any]
 
 
+def unseralize_object(cls, value):
+    if isinstance(cls, type(NamedTuple)):
+        return create_object_hook(cls)(value)
+    if isinstance(value, dict):
+        return cls(**value)
+    return cls(value)
+
+
 def create_object_hook(
     named_tuple_cls: Type[NamedTuple_T]
 ) -> Callable[[Json_Object], NamedTuple_T]:
     """Return a Object hook to seralize the given named tuple class from JSON"""
+
+    def create_object_hook(obj: Json_Object) -> NamedTuple_T:
+        created_object = dict()
+        for key, value_cls in named_tuple_cls._field_types.items():
+            created_object[key] = unseralize_object(value_cls, obj[key])
+        return named_tuple_cls._make(created_object)
+
+    return create_object_hook
