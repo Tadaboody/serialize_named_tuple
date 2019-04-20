17d1cdfd40ea840a8fac0ae18f14db6ac4b1d43c
Tomer Keren <tomer.keren.dev@gmail.com>
2019-04-21 00:27:31 +0300

    Add tests for a more deeply-nested tuple

--- test_serailize.py
+++ test_serailize.py
@@ -14,6 +14,11 @@ class NestedTuple(typing.NamedTuple):
     data: FlatTuple
 
 
+class DoubleyNestedTuple(typing.NamedTuple):
+    name: str
+    data: NestedTuple
+
+
 def test_seralize_flat_type():
     flat_tuple_json = '{"x":0,"y":1}'
     loaded = json.loads(flat_tuple_json)
@@ -31,3 +36,14 @@ def test_seralize_nested_type():
     assert type(typed_loaded.name) == str
     assert type(typed_loaded.data) == FlatTuple
     assert type(typed_loaded.data.x) == int
+
+
+def test_doubley_nested_type():
+    doubly_nested_json = '{"name":"hey","data":{"name":"arnold","data":{"x":0,"y":1}}}'
+    loaded = json.loads(doubly_nested_json)
+    typed_loaded = serialize_named_tuples.make_typed(DoubleyNestedTuple, loaded)
+    assert type(typed_loaded) == DoubleyNestedTuple
+    assert type(typed_loaded.data) == NestedTuple
+    assert type(typed_loaded.data.name) == str
+    assert type(typed_loaded.data.data) == FlatTuple
+    assert type(typed_loaded.data.data.x) == int
