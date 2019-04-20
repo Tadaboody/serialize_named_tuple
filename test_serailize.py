097cb50717e72e3e77c71113757c5584c57db536
Tomer Keren <tomer.keren.dev@gmail.com>
2019-04-20 23:50:27 +0300

    Rename test clas to show their case, flat/nested instead of inner/outer

--- test_serailize.py
+++ test_serailize.py
@@ -4,20 +4,20 @@ import json
 import typing
 
 
-class InnerItem(typing.NamedTuple):
+class FlatTuple(typing.NamedTuple):
     x: int
     y: int
 
 
-class OuterItem(typing.NamedTuple):
+class NestedTuple(typing.NamedTuple):
     name: str
-    data: InnerItem
+    data: FlatTuple
 
 
-def test_seralize_inner_type():
-    inner_item_json = '{"x":0,"y":1}'
+def test_seralize_flat_type():
+    flat_tuple_json = '{"x":0,"y":1}'
     loaded = json.loads(
-        inner_item_json,
-        object_hook=serialize_named_tuples.create_object_hook(InnerItem),
+        flat_tuple_json,
+        object_hook=serialize_named_tuples.create_object_hook(FlatTuple),
     )
-    assert type(loaded) == InnerItem
+    assert type(loaded) == FlatTuple
