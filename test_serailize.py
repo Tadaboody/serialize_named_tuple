fd02c8ba8dc1569ca2013886cd12063bf4bd74df
Tomer Keren <tomer.keren.dev@gmail.com>
2019-04-20 23:47:44 +0300

    Fix test JSON formatting

--- test_serailize.py
+++ test_serailize.py
@@ -15,9 +15,9 @@ class OuterItem(typing.NamedTuple):
 
 
 def test_seralize_inner_type():
-    inner_item_json = "{'x':0,'y':1}"
+    inner_item_json = '{"x":0,"y":1}'
     loaded = json.loads(
         inner_item_json,
         object_hook=serialize_named_tuples.create_object_hook(InnerItem),
     )
-    assert type(loaded) == custom_named_tuples.InnerItem
+    assert type(loaded) == InnerItem
